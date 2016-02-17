b65ccc9ebd59dd6bba8d4224bab1c747c06b4ffc
Stefan Zerkalica <zerkalica@gmail.com>
2016-02-17 18:29:50 +0300

    fix

--- src/common/components/LoadingPage.js
+++ src/common/components/LoadingPage.js
@@ -7,7 +7,7 @@ type LoadingPageProps = {
 export default class LoadingPage extends Component<void, LoadingPageProps, void> {
     render(): ReactElement { // eslint-disable-line
         const {
-        } = this.props
+    } = this.props
 
         return (
             <div className="page-loading">
